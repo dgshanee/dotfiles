262b3fbbf59297ef43f7d2d15e7244a6b78a932c
Shane Ciardelli <135490943+dgshanee@users.noreply.github.com>
2026-07-16 16:06:28 -0500

    keybinds and lsp

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -1,7 +1,29 @@
-require("lspconfig").clangd.setup({
-  root_dir = require("lspconfig").util.root_pattern(
-    ".clangd",
-    "compile_commands.json",
-    ".git"
-  ),
+local lspconfig = require("lspconfig")
+
+vim.lsp.config["lua_ls"] = {
+	-- Command and arguments to start the server.
+	cmd = { "lua-language-server" },
+	-- Filetypes to automatically attach to.
+	filetypes = { "lua" },
+	-- Sets the "workspace" to the directory where any of these files is found.
+	-- Files that share a root directory will reuse the LSP server connection.
+	-- Nested lists indicate equal priority, see |vim.lsp.Config|.
+	root_markers = { { ".luarc.json", ".luarc.jsonc" }, ".git" },
+	-- Specific settings to send to the server. The schema is server-defined.
+	-- Example: https://raw.githubusercontent.com/LuaLS/vscode-lua/master/setting/schema.json
+	settings = {
+		Lua = {
+			runtime = {
+				version = "LuaJIT",
+			},
+		},
+	},
+}
+
+lspconfig.clangd.setup({
+	root_dir = require("lspconfig").util.root_pattern(".clangd", "compile_commands.json", ".git"),
 })
+
+lspconfig.pyright.setup({})
+
+lspconfig.jdtls.setup({})
